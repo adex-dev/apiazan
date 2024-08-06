477020da5f18e8c8769c60f72290fa85f1ff6b9a
adex-dev <akmadnudin.inc@gmail.com>
2024-08-06 09:56:26 +0700

    Pesan commit Azan by bot ~4,2-~0,4

--- cronjobazan.bat
+++ cronjobazan.bat
@@ -11,12 +11,15 @@ set datetime=%localdatetime%
 set year=%datetime:~0,4%
 set month=%datetime:~4,2%
 
+REM Format tanggal dalam format YYYY-MM
+set formatted_date=%year%-%month%
+
 REM Tambahkan semua perubahan ke staging area
 git add .
 
 REM Buat commit dengan pesan
 
-git commit -m "Pesan commit Azan by bot !month!-!year!"
+git commit -m "Pesan commit Azan by bot %month%-%year% "
 
 REM Push perubahan ke remote repository
 git push origin main
